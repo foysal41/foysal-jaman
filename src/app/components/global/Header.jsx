207e0c979f829265340ed69e061b874c493d399c
foysal41 <mdfoysal.mf33@gmail.com>
2026-08-08 19:05:44 +0600

    adjustment header alignment

--- src/app/components/global/Header.jsx
+++ src/app/components/global/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
 
   return (
     <header className="fixed left-0 top-0 z-50 w-full border-b border-gray-100 bg-white/95 shadow-[0_2px_10px_rgba(0,0,0,0.06)] backdrop-blur-sm">
-      <div className="mx-auto flex max-w-325 items-center justify-between px-4 py-5 md:px-8">
+      <div className="mx-auto flex max-w-325 items-center justify-between px-4 md:px-0 py-5 ">
 
         {/* LOGO */}
         <Link
